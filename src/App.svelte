721138022603927f94b94d9a5e1018f2e5ca8439
JoseDv1 <Jose123millon@gmail.com>
2024-09-26 21:51:47 -0500

    REBOOT

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,47 @@
 <script lang="ts">
-	// Imports
-	import AddTaskForm from "./lib/components/AddTaskForm.svelte";
-	import TaskList from "./lib/components/TaskList.svelte";
-	import Timer from "./lib/components/Timer.svelte";
-	import { onMount } from "svelte";
-
-	onMount(() => {
-		// Pedir permiso para mostrar notificaciones
-		Notification.requestPermission();
-	});
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
 </script>
 
-<h1>Pomodoro App</h1>
 <main>
-	<!-- Cuenta atras -->
-	<Timer />
-	<!-- Lista de tareas -->
-	<TaskList />
-	<!-- POPUP Formulario -->
-	<AddTaskForm />
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
 </main>
 
 <style>
-	main {
-		display: grid;
-		grid-template-columns: 30% 1fr;
-		grid-template-rows: 1fr;
-		grid-gap: 1rem;
-		height: 100dvh;
-		max-height: calc(100vh - 5rem);
-	}
-
-	@media (width < 1300px) {
-		h1 {
-			text-align: center;
-		}
-
-		main {
-			display: flex;
-			gap: 1rem;
-			flex-direction: column;
-			height: auto;
-			max-height: none;
-		}
-	}
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import './app.css'
 import App from './App.svelte'
 
-let app: App | null = null;
-const targetElement = document.getElementById('app');
+const app = new App({
+  target: document.getElementById('app')!,
+})
 
-if (targetElement) {
-  app = new App({
-    target: targetElement,
-  });
-} else {
-  console.error('Target element not found');
-}
-
-export default app;
+export default app
